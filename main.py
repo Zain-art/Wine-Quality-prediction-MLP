ec6454534ca0ef79800e5bea6e4d8df1fb9a6499
Zian-art <za870411@gmail.com>
2024-07-25 11:05:18 +0500

    logs create file and folder

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-from src.wine_qaulity_ml_project.config import configuration # type: ignore
+# from src.wine_qaulity_ml_project.config import configuration # type: ignore
 # from src.wine-qaulity-ml-project.config import configuration # type: ignore
+
+from src.wine_qaulity_ml_project.logging import logger
+
+logger.info('this is out custom logs!.... zaki')
